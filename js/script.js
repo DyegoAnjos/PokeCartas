f888a0c07c25c285fb93d7bc2f048b91161e9959
Dyego Anjos <107328787+DyegoAnjos@users.noreply.github.com>
2024-10-17 20:13:34 -0300

    Lógica de combate

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const box_meio = document.querySelector("#meio")
+const box_lado1 = document.querySelector("#lado1")
+const box_lado2 = document.querySelector("#lado2")
 const card = document.querySelectorAll(".card")
 const card_id_pokemon = document.querySelectorAll(".card_id_pokemon")
 const card_hp_value = document.querySelectorAll(".value_hp_pokemon")
@@ -6,13 +9,17 @@ const card_name_pokemon = document.querySelectorAll(".card_name_pokemon")
 const card_box_status_pokemon = document.querySelectorAll(".box_status_pokemon")
 const card_box_type_pokemon = document.querySelectorAll(".card_box_type_pokemon")
 const box_hp_pokemon = document.querySelectorAll(".box_hp_pokemon")
+const button_finalizar = document.querySelector("#button_finalizar")
+const sumario = document.querySelector("#sumario")
 
+const escolhaJogadores = [];
+const valoresJogadores = [];
+var rodada = 0;
 
 document.addEventListener('DOMContentLoaded', function() {
     for(i=0;i < card.length;i++){
         fetchPokemon(getRandomInteger(1, 1025), i);
     }
-    
 });
 
 
@@ -88,6 +95,8 @@ function displayPokemonStats(pokemon, cod_card){
         }
     }
 }
+
+
 
 function normalizar(stat, limiteNormalizar, i){
     return Math.round(((stat - 1)/i * (limiteNormalizar-1) + 1))
@@ -171,3 +180,104 @@ const typesColors = [
         color: "#e62829"
     },
 ]
+
+card.forEach(cardElement => {
+    cardElement.addEventListener('click', () => {
+        cardElement.classList.add('selecionado')
+    });
+});
+
+// Itera sobre cada .box_status_pokemon
+card_box_status_pokemon.forEach(boxStatusContainer => {
+    // Pega o enésimo filho de cada .box_status_pokemon
+    const enesimoFilho = boxStatusContainer.children[0]; // Substitua 2 pelo índice do filho desejado
+    enesimoFilho.addEventListener('click', () => {
+        escolhaJogadores[rodada] = 0
+        valoresJogadores[rodada] = Number(boxStatusContainer.children[0].innerText.match(/\d+/g))
+    });
+});
+
+card_box_status_pokemon.forEach(boxStatusContainer => {
+    // Pega o enésimo filho de cada .box_status_pokemon
+    const enesimoFilho = boxStatusContainer.children[1]; // Substitua 2 pelo índice do filho desejado
+    enesimoFilho.addEventListener('click', () => {
+        escolhaJogadores[rodada] = 1
+        valoresJogadores[rodada] = Number(boxStatusContainer.children[1].innerText.match(/\d+/g))
+    });
+});
+
+card_box_status_pokemon.forEach(boxStatusContainer => {
+    // Pega o enésimo filho de cada .box_status_pokemon
+    const enesimoFilho = boxStatusContainer.children[2]; // Substitua 2 pelo índice do filho desejado
+    enesimoFilho.addEventListener('click', () => {
+        escolhaJogadores[rodada] = 2
+        valoresJogadores[rodada] = Number(boxStatusContainer.children[2].innerText.match(/\d+/g))
+    });
+});
+
+button_finalizar.addEventListener('click', () =>{
+    rodada++
+    sumario.innerHTML = "Ação do Jogador 2 <br> Selecione a ação <br>e aperte em finalizar"
+    if(rodada > 1){
+        rodada = 0
+        batalhar()
+        sumario.innerHTML = "Ação do Jogador 1 <br> Selecione a ação <br>e aperte em finalizar"
+
+    }
+})
+
+function batalhar(){
+    let vencedor=Pedra_Papel_Tesoura(escolhaJogadores[0], escolhaJogadores[1])
+    if(vencedor === "empate"){
+        for(i=0;i<box_lado1.children.length;i++){
+            if (box_lado1.children[i].classList.contains("selecionado")) {
+                let cardSelecionado = box_lado1.children[i];
+                let hp_card_dano = cardSelecionado.querySelector('.value_hp_pokemon'); // Seleciona o elemento .value_hp_pokemon dentro do card selecionado
+                
+                hp_card_dano.innerText = Number(hp_card_dano.innerText) - valoresJogadores[1]
+            }
+
+            if (box_lado2.children[i].classList.contains("selecionado")) {
+                let cardSelecionado = box_lado2.children[i];
+                let hp_card_dano = cardSelecionado.querySelector('.value_hp_pokemon'); // Seleciona o elemento .value_hp_pokemon dentro do card selecionado
+                
+                hp_card_dano.innerText = Number(hp_card_dano.innerText) - valoresJogadores[0]
+            }
+        }
+    }
+
+    else if(escolhaJogadores[0] === vencedor){
+        for(i=0;i<box_lado2.children.length;i++){
+            if (box_lado2.children[i].classList.contains("selecionado")) {
+                let cardSelecionado = box_lado2.children[i];
+                let hp_card_dano = cardSelecionado.querySelector('.value_hp_pokemon'); // Seleciona o elemento .value_hp_pokemon dentro do card selecionado
+                
+                hp_card_dano.innerText = Number(hp_card_dano.innerText) - valoresJogadores[0]
+            }
+        }
+    }
+
+    else{
+        for(i=0;i<box_lado1.children.length;i++){
+            if (box_lado1.children[i].classList.contains("selecionado")) {
+                let cardSelecionado = box_lado1.children[i];
+                let hp_card_dano = cardSelecionado.querySelector('.value_hp_pokemon'); // Seleciona o elemento .value_hp_pokemon dentro do card selecionado
+                
+                hp_card_dano.innerText = Number(hp_card_dano.innerText) - valoresJogadores[1]
+            }
+        }
+    }
+
+}
+
+function Pedra_Papel_Tesoura(escola1, escolha2){
+    if(escola1 === escolha2)
+        return "empate"
+
+    else if((escola1 === 0 && escolha2 === 1)||(escola1 === 1 && escolha2 === 0))
+        return 1
+    else if((escola1 === 1 && escolha2 === 2)||(escola1 === 2 && escolha2 === 1))
+        return 2
+    else
+        return 0
+}
